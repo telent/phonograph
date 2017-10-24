5f8deeb90b7048152ab79a2d7735064a4af55305
Daniel Barlow <dan@telent.net>
2017-10-24 01:14:08 +0100

    arq "hello world": write and query a triple
    phonograph.db=> (update-dataset @ds "insert data { <http://a.com> <http://b.com> <http://c.com> } ")
    phonograph.db=> (select-from-dataset @ds "select *  { ?a ?b ?c }")
    [{"c" #object[java.net.URI 0x48415178 "http://c.com"], "b" #object[java.net.URI 0x359d98ad "http://b.com"], "a" #object[java.net.URI 0x6c8ba37e "http://a.com"]}]

--- src/phonograph/db.clj
+++ src/phonograph/db.clj
@@ -1,8 +1,14 @@
 (ns phonograph.db
   (:require [clojure.java.io :as io])
-  (:import (org.apache.jena.query DatasetFactory Dataset)
+  (:import (org.apache.jena.query
+            DatasetFactory Dataset
+            QueryExecution
+            QueryExecutionFactory
+            QueryFactory)
+           (java.net URI)
            (org.apache.jena.rdf.model Model)
            (org.apache.jena.tdb TDBFactory)
+           (org.apache.jena.update UpdateAction)
            #_ (org.apache.jena.fuseki.embedded FusekiServer)))
 
 (defn open-dataset []
@@ -11,7 +17,39 @@
 
 (def ds (atom nil))
 
+(defn graph-size-in [ds]
+  (.size (.getGraph (.getDefaultModel ds))))
+
+
+(defn update-dataset [dataset s]
+  (UpdateAction/parseExecute s dataset))
+
+(defn futz [ri]
+  (cond (.isLiteral ri) (.asLiteral ri)
+        (.isURIResource ri) (URI. (.getURI (.asResource ri)))
+        (.isResource ri) (.asResource ri)
+        :else ri))
+
+
+(defn collect-results [rs]
+  (when (.hasNext rs)
+    (let [sol (.next rs)
+          names (iterator-seq (.varNames sol))]
+      (conj (or (collect-results rs) [])
+            (reduce (fn [m n] (assoc m n (futz (.get sol n))))
+                    {}
+                    names)))))
+
+(defn select-from-dataset [dataset s]
+  (let [q (QueryFactory/create s)
+        m (.getDefaultModel dataset)
+        qe (QueryExecutionFactory/create q m)]
+    (try (let [rs (.execSelect qe)]
+           (doall (collect-results rs))))))
+
+
 (reset! ds (open-dataset))
+
 
 #_
 (defn open-server [ds]
